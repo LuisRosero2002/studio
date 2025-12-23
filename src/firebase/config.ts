ee507879220939c8c75364c338087bb77dfebb9c
Luis Rosero <luis.rosero@wigausa.com>
2025-12-23 21:03:25 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCMvSGGrAPlRWptvk95NZVt91Qm_SGf3Ec",
-  authDomain: "wisales-cdeed.firebaseapp.com",
-  projectId: "wisales-cdeed",
-  storageBucket: "wisales-cdeed.appspot.com",
-  messagingSenderId: "737669429674",
-  appId: "1:737669429674:web:6740a3aa5d9f86d50153d3",
-  measurementId: "G-NSR7YN4J1T"
+  "projectId": "wisales-cdeed",
+  "appId": "1:737669429674:web:ffb7f59943bfb1ad0153d3",
+  "apiKey": "AIzaSyCMvSGGrAPlRWptvk95NZVt91Qm_SGf3Ec",
+  "authDomain": "wisales-cdeed.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "737669429674"
 };
