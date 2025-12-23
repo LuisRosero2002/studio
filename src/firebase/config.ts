9c9089ed6c80a1e993655dc8f6a010967e96e90e
Luis Rosero <luiscrt2002@gmail.com>
2025-12-23 20:30:02 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-1709482735-59d9b",
-  "appId": "1:524646834388:web:81761f7395af0aa678db41",
-  "apiKey": "AIzaSyCCx0Ne55oNnMxYetD89NaU8ZHNGPIRJzk",
-  "authDomain": "studio-1709482735-59d9b.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "524646834388"
+  apiKey: "AIzaSyCMvSGGrAPlRWptvk95NZVt91Qm_SGf3Ec",
+  authDomain: "wisales-cdeed.firebaseapp.com",
+  projectId: "wisales-cdeed",
+  storageBucket: "wisales-cdeed.appspot.com",
+  messagingSenderId: "737669429674",
+  appId: "1:737669429674:web:6740a3aa5d9f86d50153d3",
+  measurementId: "G-NSR7YN4J1T"
 };
